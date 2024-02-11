9694b911619f445582d1bda0dea9d7acf5bae1a2
Alireza Atashnejad <32798129+Mratashnejad@users.noreply.github.com>
2024-02-11 10:17:24 +0400

    update

--- app.js
+++ app.js
@@ -1,56 +1,24 @@
-const uuid = require('uuid');
-console.log(uuid.v4());
+const express = require('express');
+require('dotenv').config();
+const app = express();
 
-// const path = require('node:path')
-// const data = path.parse(__filename)
-// console.log(data)
+//routing
 
-// const os = require('node:os');
-// const total = os.totalmem()
-// const free = os.freemem();
+app.get('/' ,(req , res)=>{
+    res.send("hello from alireza")
 
-// console.log(total , free)
+})
 
-
-// const fs = require('node:fs');
-// const files = fs.readdirSync('./')
-// console.log(files);
-
-// fs.readdir('./' , (err , files)=>{
-
-//     if(err) console.log(`error : ${err}`)
-//     else console.log(files)
-// })
+app.get('/api/courses' , (req , res)=>{
+    res.send(['html' , 'css' , 'java'])
+})
 
 
+app.get('/api/courses/:courseid/:name?',(req,res)=>{
+    res.send([req.params.courseid , req.params.name , req.query.sort])
+})
 
-// const EventEmitter = require('node:events');
-// const emitter = new EventEmitter();
-
-// emitter.on('messageLogged',(data)=>{
-//     console.log('listner called' , data)
-// });
-
-
-// emitter.emit('messageLogged' , {id:1 , name:"alireza" , lastname:"atashnejad"});
-
-
-// const http= require('http');
-
-// const server = http.createServer((req , res)=>{
-// if (req.url === "/"){
-//     res.write("hello from home page");
-//     res.end()
-// }
-// if(req.url ==='/api/course'){
-//     res.write(JSON.stringify(['html' ,'css']))
-//     res.end();
-// }
-// });
-
-// server.on("connection" , (socket)=>{
-//     console.log('new connections is connected');
-// })
-
-// server.listen(3000);
-// console.log("server is listinign on port 3000");
+const port = process.env.PORT || 3000
+app.listen(port , ()=>{
+    console.log(`listening port ${port}`)
+})
